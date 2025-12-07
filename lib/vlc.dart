dba4367fb4f887d9ae38a31f205ebbb95fbc7f48
kybix5 <80044438+kybix5@users.noreply.github.com>
2025-12-07 23:08:34 +0300

    Update vlc.dart2

--- lib/vlc.dart
+++ lib/vlc.dart
@@ -1,103 +1,133 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-class CameraScreenWidget extends StatefulWidget {
-  const CameraScreenWidget({super.key});
-
+class FeedScreenWidget extends StatefulWidget {
   @override
-  _CameraScreenWidgetState createState() => _CameraScreenWidgetState();
+  _FeedScreenWidgetState createState() => _FeedScreenWidgetState();
 }
 
-class _CameraScreenWidgetState extends State<CameraScreenWidget> {
-  bool _isPlaying = true;
-  bool _isError = false; // Новое состояние для отслеживания ошибок
+class _FeedScreenWidgetState extends State<FeedScreenWidget> {
+  var size_n, height_n, width_n;
+  List tableObjsJson = [];
 
-  final VlcPlayerController _videoPlayerController = VlcPlayerController.network(
-    //'rtsp://46.16.226.6:554/user=user&password=&channel=1&stream=0',
-    'http://46.16.226.6:8090',
-    //'http://media.w3.org/2010/05/bunny/movie.mp4',
-    hwAcc: HwAcc.full,
-    autoPlay: true,
-    options: VlcPlayerOptions(),
-  );
+  Future<String> download() async {
+    try {
+      var request = await HttpClient().getUrl(
+          Uri.parse('https://anchih.e-rec.ru/api/feed/get_feed.php'));
+      var response = await request.close();
 
-  Color _buttonColorPlay = Colors.green;
-  Color _buttonColorPause = Colors.black;
-
-  @override
-  void initState() {
-    super.initState();
-    // Добавляем слушателя для отслеживания состояния потока
-    _videoPlayerController.addListener(() {
-      if (_videoPlayerController.value.hasError) {
-        setState(() {
-          _isError = true; // Устанавливаем состояние ошибки
-        });
+      await for (var contents in response.transform(Utf8Decoder())) {
+        tableObjsJson = jsonDecode(contents) as List;
       }
-    });
-  }
+    } catch (e) {
+      print("Ошибка загрузки: $e");
+    }
 
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    super.dispose();
-  }
-
-  void _playVideo() {
-    setState(() {
-      _isError = false; // Сбрасываем состояние ошибки при попытке воспроизведения
-      _buttonColorPlay = Colors.green;
-      _buttonColorPause = Colors.black;
-      _isPlaying = true;
-      _videoPlayerController.play();
-    });
-  }
-
-  void _pauseVideo() {
-    setState(() {
-      _buttonColorPause = Colors.green;
-      _buttonColorPlay = Colors.black;
-      _isPlaying = false;
-      _videoPlayerController.pause();
-    });
+    return Future.value("Data download");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Камера')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_isError) // Если есть ошибка, показываем сообщение
-              const Text(
-                'Ошибка загрузки видео',
-                style: TextStyle(color: Colors.red, fontSize: 20),
-              )
-            else
-              Center(
-                child: VlcPlayer(
-                  controller: _videoPlayerController,
-                  aspectRatio: 16 / 9,
-                  placeholder: const Center(child: CircularProgressIndicator()),
-                ),
-              ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: _isError ? null : _playVideo, // Деактивируем кнопку воспроизведения при ошибке
-                  child: Icon(Icons.play_arrow, size: 28, color: _buttonColorPlay),
-                ),
-                TextButton(
-                  onPressed: _isError ? null : _pauseVideo, // Деактивируем кнопку паузы при ошибке
-                  child: Icon(Icons.pause, size: 28, color: _buttonColorPause),
-                ),
-              ],
+    size_n = MediaQuery.of(context).size;
+    height_n = size_n.height;
+    width_n = size_n.width;
+
+    return FutureBuilder<String>(
+      future: download(),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: Text('Идет загрузка...'));
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          return Scaffold(
+            appBar: AppBar(title: Text('Лента')),
+            body: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: tableObjsJson.length,
+              itemBuilder: (BuildContext context, int index) {
+                var item = tableObjsJson[index];
+                return FeedCard(item: item, height_n: height_n);
+              },
             ),
-          ],
-        ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class FeedCard extends StatefulWidget {
+  final Map item;
+  final double height_n;
+  FeedCard({required this.item, required this.height_n});
+
+  @override
+  _FeedCardState createState() => _FeedCardState();
+}
+
+class _FeedCardState extends State<FeedCard> {
+  VlcPlayerController? _vlcController;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.item['url_feed'].toString().endsWith('.mp4') ||
+        widget.item['url_feed'].toString().contains('http')) {
+      _vlcController = VlcPlayerController.network(
+        widget.item['url_feed'],
+        hwAcc: HwAcc.FULL,
+        autoPlay: true,
+        options: VlcPlayerOptions(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _vlcController?.stop();
+    _vlcController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          widget.item['url_feed'].toString().endsWith('.mp4')
+              ? (_vlcController != null
+                  ? Container(
+                      height: widget.height_n / 3,
+                      child: VlcPlayer(
+                        controller: _vlcController!,
+                        aspectRatio: 16 / 9,
+                        placeholder: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    )
+                  : Container(
+                      height: widget.height_n / 3,
+                      child: Center(child: CircularProgressIndicator())))
+              : Image.network(
+                  widget.item['url_feed'],
+                  fit: BoxFit.cover,
+                  height: widget.height_n / 3,
+                  width: double.infinity,
+                ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              widget.item['description'] ?? '',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
       ),
     );
   }
