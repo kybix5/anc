f51aa9cb1493f436dcb07482d44a464c44c43320
kybix5 <80044438+kybix5@users.noreply.github.com>
2025-12-08 01:02:39 +0300

    вернулся

--- lib/massege.dart
+++ lib/massege.dart
@@ -13,173 +13,196 @@ class MessageWidget extends StatefulWidget {
 
 class _MessageWidgetState extends State<MessageWidget> {
   String deviceId = 'unknown';
-  final TextEditingController _controller = TextEditingController();
-  final String _key = 'my32lengthsupersecretnooneknows1'; // 32 байта
 
+  final TextEditingController _controller = TextEditingController();
+
+  final String _key = 'my32lengthsupersecretnooneknows1'; // 32 байта
   List<String> messages = [];
   List<String> sender = [];
   List<String> created = [];
   final ScrollController _scrollController = ScrollController();
 
-  @override
-  void initState() {
-    super.initState();
-    getDeviceId();
-    _fetchMessages();
-  }
+  Future getDeviceId() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    //String? deviceId;
 
-  Future<void> getDeviceId() async {
-    try {
+    // Получаем информацию о платформе
+    if (Theme.of(context).platform == TargetPlatform.android) {
       DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-      if (Theme.of(context).platform == TargetPlatform.android) {
-        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-        deviceId = androidInfo.id ?? 'unknown';
-      } else if (Theme.of(context).platform == TargetPlatform.iOS) {
-        IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-        deviceId = iosInfo.identifierForVendor ?? 'unknown';
-      }
-      print('Device ID: $deviceId');
-    } catch (e) {
-      print('Ошибка получения Device ID: $e');
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      String dev = androidInfo.toString();
+      //print(dev);
+      deviceId = androidInfo.id;
+      // Уникальный идентификатор для Android
+    } else if (Theme.of(context).platform == TargetPlatform.iOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      deviceId = iosInfo.identifierForVendor
+          .toString(); // Уникальный идентификатор для iOS
     }
+    print('Device ID: $deviceId');
   }
 
   void _sendMessage(String message) async {
-    if (message.isEmpty) return;
-
+    if (message.isEmpty) return; // Проверка на пустое сообщение
+    const senderIn = 'Вы';
     final createdAt = DateTime.now().toIso8601String().split('.')[0];
     final iv = encrypt.IV.fromLength(16);
+    //final iv = IV.fromSecureRandom(16);
     final encrypter =
         encrypt.Encrypter(encrypt.AES(encrypt.Key.fromUtf8(_key)));
 
+    // Шифрование сообщения
     final encrypted = encrypter.encrypt(message, iv: iv);
 
-    try {
-      await http.post(
-        Uri.parse('https://anchih.e-rec.ru/api/store_message.php'),
-        body: json.encode({
-          'message': encrypted.base64,
-          'iv': iv.base64,
-          'sender': deviceId,
-        }),
-        headers: {'Content-Type': 'application/json'},
-      );
+    print(encrypted.base64);
+    print('--');
+    print(iv.base64);
 
-      if (!mounted) return;
+    // getDeviceId();
 
-      setState(() {
-        sender.add('вы');
-        messages.add(message);
-        created.add(createdAt);
-      });
+    // Отправка на сервер
+    await http.post(
+      Uri.parse('https://anchih.e-rec.ru/api/store_message.php'),
+      body: json.encode({
+        'message': encrypted.base64,
+        'iv': iv.base64,
+        'sender': deviceId,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
 
-      _scrollToBottom();
-    } catch (e) {
-      print('Ошибка отправки сообщения: $e');
-    }
+    setState(() {
+      sender.add(senderIn);
+      messages.add(message);
+      created.add(createdAt);
+    });
+
+    // Прокрутка вниз к последнему сообщению
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+
+    // Прокрутка вниз после отправки сообщения
+    _scrollToBottom();
   }
 
   void _fetchMessages() async {
-    try {
-      final response = await http
-          .get(Uri.parse('https://anchih.e-rec.ru/api/get_messages.php'));
+    final response = await http
+        .get(Uri.parse('https://anchih.e-rec.ru/api/get_messages.php'));
 
-      if (!mounted) return;
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonMessages = json.decode(response.body);
+      for (var jsonMessage in jsonMessages) {
+        final iv = encrypt.IV.fromBase64(jsonMessage['iv']);
+        final encryptedMessage = jsonMessage['message'];
+        final senderIn = jsonMessage['sender'];
+        final createdAt = jsonMessage['created_at'];
 
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonMessages = json.decode(response.body);
+        // Дешифрование сообщения
+        final encrypter =
+            encrypt.Encrypter(encrypt.AES(encrypt.Key.fromUtf8(_key)));
+        //final decrypted = encrypter.decrypt64(encryptedMessage, iv: iv);
 
-        if (jsonMessages.isEmpty) {
-          print('Сообщений нет');
-          setState(() {}); // чтобы Center("Нет сообщений") отобразился
-          return;
+        try {
+          final decrypted = encrypter.decrypt64(encryptedMessage, iv: iv);
+          setState(() {
+            //если автором являеться это устройство то отмечаем
+            if (senderIn == deviceId) {
+              sender.add("вы");
+            } else {
+              sender.add(senderIn);
+            }
+            messages.add(decrypted);
+            created.add(createdAt);
+          });
+        } catch (e) {
+          print('Ошибка дешифрования: $e');
         }
-
-        for (var jsonMessage in jsonMessages) {
-          final iv = encrypt.IV.fromBase64(jsonMessage['iv']);
-          final encryptedMessage = jsonMessage['message'];
-          final senderIn = jsonMessage['sender'];
-          final createdAt = jsonMessage['created_at'];
-
-          final encrypter =
-              encrypt.Encrypter(encrypt.AES(encrypt.Key.fromUtf8(_key)));
-
-          try {
-            final decrypted = encrypter.decrypt64(encryptedMessage, iv: iv);
-
-            if (!mounted) return;
-
-            setState(() {
-              sender.add(senderIn == deviceId ? 'вы' : senderIn);
-              messages.add(decrypted);
-              created.add(createdAt);
-            });
-          } catch (e) {
-            print('Ошибка дешифрования: $e');
-          }
-        }
-
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          _scrollToBottom();
-        });
-      } else {
-        print('Ошибка сервера: ${response.statusCode}');
       }
-    } catch (e) {
-      print('Ошибка запроса: $e');
     }
+
+    // Прокрутка вниз после завершения всех обновлений состояния
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
   }
 
   void _scrollToBottom() {
-    if (!mounted) return;
-    if (_scrollController.hasClients && messages.isNotEmpty) {
+    if (_scrollController.hasClients) {
       _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
     }
+  }
+
+/*
+  @override
+  void initState() {
+    super.initState();
+    _fetchMessages();
+  }
+*/
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _fetchMessages();
+    getDeviceId();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Сообщения')),
+      appBar: AppBar(title: const Text('Сообшения')),
       body: Column(
         children: [
           Expanded(
-            child: messages.isEmpty
-                ? const Center(child: Text('Нет сообщений'))
-                : ListView.builder(
-                    controller: _scrollController,
-                    padding: const EdgeInsets.all(8),
-                    itemCount: messages.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(
-                            sender[index],
-                            style: const TextStyle(
-                                fontSize: 10, color: Colors.blue),
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+            child: ListView.builder(
+                controller: _scrollController,
+                padding: const EdgeInsets.all(8),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(
+                        sender[index],
+                        style:
+                            const TextStyle(fontSize: 10, color: Colors.blue),
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                messages[index],
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.black),
-                              ),
-                              Align(
-                                alignment: Alignment.centerRight,
+                              Expanded(
                                 child: Text(
-                                  created[index],
+                                  messages[index], // Время
                                   style: const TextStyle(
-                                      fontSize: 8, color: Colors.grey),
+                                      fontSize: 14, color: Colors.black),
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      );
-                    },
-                  ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '', // Время
+                                style:
+                                    TextStyle(fontSize: 8, color: Colors.grey),
+                              ),
+                              Text(
+                                created[index], // Дата
+                                style: const TextStyle(
+                                    fontSize: 8, color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                  );
+                }),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -188,8 +211,8 @@ class _MessageWidgetState extends State<MessageWidget> {
                 Expanded(
                   child: TextField(
                     controller: _controller,
-                    decoration: const InputDecoration(
-                        hintText: 'Введите сообщение..'),
+                    decoration:
+                        const InputDecoration(hintText: 'Введите сообщение..'),
                   ),
                 ),
                 IconButton(
@@ -212,7 +235,6 @@ class _MessageWidgetState extends State<MessageWidget> {
   @override
   void dispose() {
     _scrollController.dispose();
-    _controller.dispose();
     super.dispose();
   }
 }
